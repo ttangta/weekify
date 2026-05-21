f12aa05b378864514c80782c2d0b0e9b9c75b9e8
ttangta <kimjh6836@gmail.com>
2026-05-21 19:35:01 +0900

    refactor: 인증 API 경로 prefix 적용

--- src/main/java/com/weekify/auth/controller/AuthController.java
+++ src/main/java/com/weekify/auth/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/open-api/auth")
 public class AuthController {
     private final AuthService authService;
 
