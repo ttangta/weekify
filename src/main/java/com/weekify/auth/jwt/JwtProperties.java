a5237f6285eff7b2a9d4d55f0f00108b85b32449
ttangta <kimjh6836@gmail.com>
2026-05-21 14:53:17 +0900

    refactor: JWT 서명 키 재사용

--- src/main/java/com/weekify/auth/jwt/JwtProperties.java
+++ src/main/java/com/weekify/auth/jwt/JwtProperties.java
@@ -1,9 +1,6 @@
 package com.weekify.auth.jwt;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "jwt")
 public record JwtProperties(
